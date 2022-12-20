b23c54b3fd317900ad2271276cd04e3949ab21f6
CamilaAchuri <camilaachury.4@gmail.com>
2022-12-20 13:40:07 -0300

    add options to user

--- R/02_fringe_data.R
+++ R/02_fringe_data.R
@@ -1,11 +1,9 @@
 #' @export
 fringe_data <- function(shape_data) {
   if (is.null(shape_data)) return()
-  df <- shape_data@data
-  if (is.null(df)) return()
-  dic <- homodatum::create_dic(df)
+  dic <- homodatum::create_dic(shape_data)
   list(
-    data = data,
+    data = shape_data,
     dic = dic
   )
 }
--- inst/cdmx-shapes-app/app.R
+++ inst/cdmx-shapes-app/app.R
@@ -13,7 +13,9 @@ ui <- panelsPage(
         can_collapse = FALSE,
         width = 300,
         body =  div(
-          uiOutput("layers")
+          uiOutput("layers"),
+          uiOutput("numeric_ui"),
+          uiOutput("label_opts")
         ),
         footer =  tags$a(
           href="https://www.datasketch.co", target="blank",
@@ -30,7 +32,8 @@ ui <- panelsPage(
           uiOutput("downloads")
         ),
         body =  div(
-          uiOutput("viz_view")
+          verbatimTextOutput("debug")
+          #uiOutput("viz_view")
         )
   )
 )
@@ -56,7 +59,7 @@ server <- function(input, output, session) {
   info_url <- reactive({
     linkInfo <- url_par()$inputs$ckanConf
     if (is.null(linkInfo)) linkInfo <-  "cd29b08a-50a3-486a-9bea-12d745e2964c"
-      cdmx.shapes:::read_ckan_info(url = urlInfo, linkInfo = linkInfo)
+    cdmx.shapes:::read_ckan_info(url = urlInfo, linkInfo = linkInfo)
   })
 
   # read url from ckan ------------------------------------------------------
@@ -64,19 +67,81 @@ server <- function(input, output, session) {
   shape_info <- reactive({
     req(info_url())
     url_shape <- info_url()$url
+    unlink("down_shapes/", recursive = TRUE)
     #url_shape <- "https://datos-prueba.cdmx.gob.mx/dataset/05d66891-33f9-405c-b6a3-f29aff791c1c/resource/ace56b90-85b6-47ed-a9d6-acf413f61dda/download/ingreso_promedio_trimestral.zip"
     unzip_shape(url = url_shape, export_dir = "down_shapes")
   })
 
 
+  # show layers to plot -----------------------------------------------------
+
   output$layers <- renderUI({
     req(shape_info)
-    checkboxGroupInput("layer_id",
-                       "Capa a visualizar",
-                       shape_info()$shape_layer,
-                       selected = shape_info()$shape_layer[1])
+    radioButtons("layer_id",
+                 "Capa a visualizar",
+                 shape_info()$shape_layer,
+                 selected = shape_info()$shape_layer[1])
   })
 
+
+  # read shape --------------------------------------------------------------
+
+  shape_load <- reactive({
+    req(shape_info())
+    shape_layer <- input$layer_id
+    if (is.null(shape_layer)) shape_layer <- shape_info()$shape_layer[1]
+    suppressWarnings(
+     shape <- read_shape(shape_dsn = shape_info()$shape_dsn,
+                 shape_layer = shape_layer,
+                 shape_file = shape_info()$shape_file)
+    )
+    shape
+  })
+
+
+
+  # read data from shape ----------------------------------------------------
+
+  shape_fringe <- reactive({
+    req(shape_load())
+    cdmx.shapes::fringe_data(shape_load()@data)
+  })
+
+
+  # variables numericas a graficar
+  numeric_var <- reactive({
+    req(shape_fringe())
+    if (class(shape_load())[1] == "SpatialPointsDataFrame") return()
+    dic <- shape_fringe()$dic
+    if (nrow(dic) == 0) return()
+    dic$hdType[dic$id == "ano"] <- "Yea"
+    dic$hdType[dic$id == "id"] <- "Uid"
+    dic <- dic |>
+      dplyr::filter(hdType == "Num")
+    if (nrow(dic) == 0) return()
+    dic$label
+  })
+
+
+  output$numeric_ui <- renderUI({
+    req(numeric_var())
+    selectizeInput("numeric_id", "Variable numerica", numeric_var())
+  })
+
+  output$label_opts <- renderUI({
+    req(shape_fringe())
+    dic <-  shape_fringe()$dic
+    if (nrow(dic) == 0) return()
+    checkboxGroupInput("label_id",
+                       "Informacion del tooltip",
+                       choices = dic$label,
+                       selected = dic$label)
+  })
+
+
+  output$debug <- renderPrint({
+    numeric_var()
+  })
 
 
   output$viz_icons <- renderUI({
