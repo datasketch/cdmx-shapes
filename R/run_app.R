b63bbf6603f3f08b2c9402fd54a978d16ec9eee5
CamilaAchuri <camilaachury.4@gmail.com>
2023-01-06 16:59:16 -0300

    update url app

--- R/run_app.R
+++ R/run_app.R
@@ -1,5 +1,5 @@
 #' @export
 run_app <- function(){
-  app_file <- system.file("cdmx.shapes/app.R", package = "cdmx.shapes")
+    app_file <- system.file("cdmx-shapes-app/app.R", package = "cdmx.shapes")
   shiny::runApp(app_file, port = 3838)
 }
