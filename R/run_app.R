435e96efbb44e0dea5b45653a959041070b2d4a0
David Daza <03dazal@gmail.com>
2023-07-31 12:40:13 -0500

    Update run_app.R

--- R/run_app.R
+++ R/run_app.R
@@ -3,3 +3,9 @@ run_app <- function(){
     app_file <- system.file("cdmx-shapes-app/app.R", package = "cdmx.shapes")
   shiny::runApp(app_file, port = 3838)
 }
+
+#' @export
+run_app_ciudadano <- function(){
+    app_file <- system.file("cdmx-shapes-app-highlight/app.R", package = "cdmx.shapes")
+    shiny::runApp(app_file, port = 3838)
+}
