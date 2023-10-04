8f13028dbe92e40870198b9d5bdb5e65e68d4999
CamilaAchuri <camilaachury.4@gmail.com>
2023-10-04 19:07:09 -0300

    rename app high

--- R/run_app.R
+++ R/run_app.R
@@ -6,6 +6,6 @@ run_app <- function(){
 
 #' @export
 run_app_ciudadano <- function(){
-    app_file <- system.file("cdmx-shapes-app-highlight/app.R", package = "cdmx.shapes")
+    app_file <- system.file("cdmx-shapes-app-high/", package = "cdmx.shapes")
     shiny::runApp(app_file, port = 3838)
 }
